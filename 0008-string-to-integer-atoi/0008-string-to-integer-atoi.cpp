0908250ea67dfe946fc0a8eba236047007813fdd
Lakshita <98271965+lakshita157@users.noreply.github.com>
2024-02-02 20:45:11 +0530

    Time: 3 ms (60.74%), Space: 9.6 MB (6.03%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        
+        stringstream geek;
+    
+        geek << s;
+    
+        int x = 0;
+        geek >> x;
+        return x;
+    
+    }
+};
