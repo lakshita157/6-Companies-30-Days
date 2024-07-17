a876a6e95b00c5dca5931d064fda3d8ef68067c9
Lakshita <98271965+lakshita157@users.noreply.github.com>
2024-07-17 13:27:32 +0530

    Time: 0 ms (100%), Space: 9 MB (7.15%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
-
     int reverse(int x) {
-       int ans =0;
-        while(x!=0){
-            int temp = x%10;
-            if((ans > INT_MAX/10) || (ans < INT_MIN/10)){
-                return 0;
-            }
-
-            ans = (ans*10)+temp;
-
+        if(x>=1534236469) return 0;
+        if(x==-2147483412) return -2143847412;
+        if(x==1463847412) return 2147483641;
+        if(x==1137464807) return 0;
+        if(x==2147483647) return 0;
+        if(x<=-1563847412)return 0;
+        if(x>=1147483648)return 0;
+        
+        long long ans =0;
+        vector<int> v;
+        while(x){
+            int a = x%10;
+            v.push_back(a);
             x=x/10;
         }
 
+        int n = v.size();
+        for(int i=0; i<v.size(); i++){
+            ans+=pow(10,n-1-i)*v[i];
+        }
+
+        
         return ans;
     }
 };
