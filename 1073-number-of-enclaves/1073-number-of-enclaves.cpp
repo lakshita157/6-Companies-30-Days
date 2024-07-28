73f78af891bd9153c090cc445fbbc36c471aac76
Lakshita <98271965+lakshita157@users.noreply.github.com>
2024-07-28 18:17:59 +0530

    Time: 55 ms (38.97%), Space: 33.2 MB (55.64%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,74 +1,88 @@
 class Solution {
 public:
-    bool check(int i, int j, int n, int m) {
-        return i < n && j < m && i >= 0 && j >= 0;
+    bool check(int i, int j, int n, int m){
+        return i<n && j<m && i>=0 && j>=0;
     }
-    
     int numEnclaves(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        queue<pair<int, int>> q;
-        int cnt = 0;
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(grid[i][j] == 1) {
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        queue<pair<int,int>>q;
+        vector<vector<int>>ans(n, vector<int>(m,0));
+        int cnt=0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j]==1){
                     cnt++;
                 }
             }
         }
 
-        // Enqueue all border cells that are land
-        for(int j = 0; j < m; j++) {
-            if(grid[0][j] == 1) {
-                q.push({0, j});
-                vis[0][j] = 1;
+        // first row;
+        for(int j=0; j<m; j++){
+            if(grid[0][j]==1){
+                q.push({0,j});
+                ans[0][j]=1;
             }
-            if(grid[n-1][j] == 1) {
+            vis[0][j]=1;
+        }
+
+        // last row
+        for(int j=0; j<m; j++){
+            if(grid[n-1][j]==1){
                 q.push({n-1, j});
-                vis[n-1][j] = 1;
+                ans[n-1][j]=1;
             }
+            vis[n-1][j]=1;
         }
-        for(int i = 0; i < n; i++) {
-            if(grid[i][0] == 1) {
-                q.push({i, 0});
-                vis[i][0] = 1;
+
+        // first col
+        for(int i=0; i<n;i++){
+            if(grid[i][0]==1){
+                q.push({i,0});
+                ans[i][0]=1;
             }
-            if(grid[i][m-1] == 1) {
+
+            vis[i][0]=1;
+        }
+
+        // last col
+        for(int i=0; i<n; i++){
+            if(grid[i][m-1]==1){
                 q.push({i, m-1});
-                vis[i][m-1] = 1;
+                ans[i][m-1]=1;
             }
+            vis[i][m-1]=1;
         }
 
-        // Directions for moving in 4 possible directions
-        int row[4] = {1, 0, 0, -1};
-        int col[4] = {0, 1, -1, 0};
-
-        while(!q.empty()) {
-            int r = q.front().first;
+        int row[4]= {1,0,0,-1};
+        int col[4] = {0,1,-1,0};
+        while(!q.empty()){
+            int r=q.front().first;
             int c = q.front().second;
             q.pop();
-            for(int i = 0; i < 4; i++) {
-                int nr = r + row[i];
-                int nc = c + col[i];
-                if(check(nr, nc, n, m) && vis[nr][nc] == 0 && grid[nr][nc] == 1) {
-                    vis[nr][nc] = 1;
-                    q.push({nr, nc});
+            for(int i=0; i<4; i++){
+                int nr = r+row[i];
+                int nc=c+col[i];
+                if(check(nr,nc,n,m) && vis[nr][nc]==0){
+                    vis[nr][nc]=1;
+                    if(grid[nr][nc]==1){
+                        ans[nr][nc]=1;
+                        q.push({nr,nc});
+                    }
                 }
             }
         }
 
-        // Count number of enclaves
-        int nc = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(grid[i][j] == 1 && vis[i][j] == 0) {
+        int nc=0;
+        for(int i=0; i<ans.size(); i++){
+            for(int j=0; j<ans[0].size(); j++){
+                if(ans[i][j]==1){
                     nc++;
                 }
             }
         }
 
-        return nc;
+        return cnt-nc;
     }
 };
