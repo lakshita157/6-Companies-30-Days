4be5725a5983ee96cd5a8205ff9f05afbb668ead
Lakshita <98271965+lakshita157@users.noreply.github.com>
2024-09-05 14:43:49 +0530

    Time: 118 ms (5.15%), Space: 117.6 MB (46.81%) - LeetHub

---
+++ 2155-find-missing-observations/2155-find-missing-observations.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        int m = rolls.size();
+        int de = n+m;
+        int sum =0;
+        for(int i=0; i<m; i++){
+            sum+=rolls[i];
+        }
+
+        vector<int>ans;
+        mean*=de;
+        mean-=sum;
+        if(mean<n || mean>6*n){
+            return {};
+        }
+
+        if(mean%n==0){
+            int x = mean/n;
+            if(x<=6){
+                while(n--){
+                    ans.push_back(x);
+                }
+                return ans; 
+            }
+
+            
+
+            else{
+                return {};
+            }
+            
+        }
+            vector<int> ans1(n, mean / n);
+        
+            
+            int remain = mean % n;
+
+          
+            for (int i = 0; i < remain; i++) {
+                ans1[i]++;
+            }
+        
+
+        return ans1;
+
+    }
+};
