add4be2e95882476a3563a222195e2e11c92bd15
Lakshita <98271965+lakshita157@users.noreply.github.com>
2024-10-18 21:08:33 +0530

    Time: 2 ms (42.21%), Space: 8.2 MB (39.62%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -3,12 +3,48 @@ public:
     vector<vector<int>> generate(int n) {
         vector<vector<int>>ans(n);
         for(int i=0; i<n; i++){
-            ans[i]=vector<int>(i+1, 1);
+            ans[i] = vector<int>(i+1, 1);
             for(int j=1; j<i; j++){
-                ans[i][j]= ans[i-1][j]+ans[i-1][j-1];
+                ans[i][j] = ans[i-1][j-1]+ans[i-1][j];
             }
         }
 
         return ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // vector<vector<int>>ans(n);
+        // for(int i=0; i<n; i++){
+        //     ans[i]=vector<int>(i+1, 1);
+        //     for(int j=1; j<i; j++){
+        //         ans[i][j]= ans[i-1][j]+ans[i-1][j-1];
+        //     }
+        // }
+
+        // return ans;
     }
 };
