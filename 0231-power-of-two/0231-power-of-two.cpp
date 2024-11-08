7f19bacc99eae47ccc3af5f20a9eb372e5c3279d
Lakshita <98271965+lakshita157@users.noreply.github.com>
2024-11-09 00:35:30 +0530

    Time: 0 ms (100%), Space: 7.5 MB (26.13%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n < 0 ? false : __builtin_popcountll(n) == 1;
+        // return n < 0 ? false : __builtin_popcountll(n) == 1;
+        for(int i=0; i<31; i++){
+            if(pow(2,i)==n)return true; 
+        }
 
+        return false;
     }
 };
