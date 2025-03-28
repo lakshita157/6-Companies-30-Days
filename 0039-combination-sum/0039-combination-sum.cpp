6b5609595c2b08b0cfa8999bc1f1cdb4e5b1451c
Lakshita <98271965+lakshita157@users.noreply.github.com>
2025-03-28 18:39:03 +0530

    Time: 45 ms (16.6%), Space: 45.1 MB (14.44%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,22 +1,25 @@
 class Solution {
-public:
-    void func(vector<int>& c, int target, int n, int sum, vector<int> v, vector<vector<int>>&ans, int indx){
-        if(sum==target ){
-            ans.push_back(v);
+public:     
+    void func(int n, int target,vector<int>& c, vector<int>v, vector<vector<int>>&ans, int indx ){
+        if(indx==n){
+            if(target==0){
+                ans.push_back(v);
+                
+            }
             return;
         }
-        if(sum>target||indx>=n)return;
-        v.push_back(c[indx]);
-        func(c, target, n, sum+c[indx], v, ans, indx);
-        v.pop_back();
-        func(c, target, n, sum, v, ans, indx+1);
+        if(c[indx]<=target){
+            v.push_back(c[indx]);
+            func(n, target-c[indx], c, v, ans, indx);
+            v.pop_back();
+        }
+        func(n, target, c, v, ans, indx+1);
     }
     vector<vector<int>> combinationSum(vector<int>& c, int target) {
-        vector<vector<int> > ans;
-        vector<int> v;
         int n = c.size();
-        func(c, target, n, 0, v, ans, 0);
+        vector<int> v;
+        vector<vector<int>>ans;
+        func(n,target,c,v,ans,0);
         return ans;
-
     }
 };
