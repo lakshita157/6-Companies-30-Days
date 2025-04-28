8b59eb95f6957c59af1fec4a2ea69d3fa9cf2ff9
Lakshita <98271965+lakshita157@users.noreply.github.com>
2025-04-28 18:39:16 +0530

    Time: 0 ms (100%), Space: 18.6 MB (79.37%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,75 +1,30 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<vector<int>>ans(matrix);
-        for(int i=0; i<matrix.size();i++){
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<pair<int,int>>v;
+        for(int i=0; i<matrix.size(); i++){
             for(int j=0; j<matrix[0].size(); j++){
                 if(matrix[i][j]==0){
-                    int x = i, y=j;
-                    for(int k=0; k<matrix[0].size(); k++){
-                        ans[x][k]=0;
-                    }
-                    for(int k = 0; k<matrix.size(); k++){
-                        ans[k][y]=0;
-                    }
+                    v.push_back({i,j});
                 }
             }
         }
 
-       
-        matrix=ans;
+        for(auto it: v){
+            int x = it.first;
+            int y = it.second;
+            for(int i=0; i<m; i++){
+                matrix[x][i]=0;
+                
+            }
 
+            for(int j=0; j<n; j++){
+                matrix[j][y]=0;
+            }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // vector<vector<int>>ans(matrix);
-        // int row = matrix.size();
-        // int col = matrix[0].size();
-
-        // for(int i=0; i<row; i++){
-        //     for(int j=0; j<col; j++){
-        //         if(matrix[i][j]==0){
-        //             int x=i;
-        //             int y=j;
-        //             for(int k=0; k<col; k++){
-        //                 ans[x][k]=0;
-        //             }
-
-        //              for(int k=0; k<row; k++){
-        //                 ans[k][y]=0;
-        //             }
-        //         }
-        //     }
-        // }
-
-        
-
-        // for(int i=0; i<row; i++){
-        //     for(int j=0; j<col; j++){
-        //         matrix[i][j] = ans[i][j];
-        //     }
-        // }
+            
+        }
     }
 };
