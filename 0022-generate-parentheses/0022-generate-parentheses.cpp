0b5499309495063f7f6b895069ad1d1d0af11715
Lakshita <98271965+lakshita157@users.noreply.github.com>
2025-06-12 12:05:16 +0530

    Time: 3 ms (71.8%), Space: 16.3 MB (21.73%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    void func(vector<string>&ans, int open,int close, string st){
-        if(open==0 &&close==0){
+    void solve(vector<string>&ans, int open, int close, string st ){
+        if(open ==0 && close ==0){
             ans.push_back(st);
             return;
         }
+
         if(open>0){
             st.push_back('(');
-            func(ans, open-1, close, st);
+            solve(ans, open-1, close, st);
             st.pop_back();
         }
         if(open<close){
             st.push_back(')');
-            func(ans, open, close-1, st);
+            solve(ans, open, close-1, st);
             st.pop_back();
         }
-
     }
     vector<string> generateParenthesis(int n) {
+        
         vector<string> ans;
         string st;
-        func(ans, n, n, st);
+        solve(ans, n, n, st);
         return ans;
     }
 };
