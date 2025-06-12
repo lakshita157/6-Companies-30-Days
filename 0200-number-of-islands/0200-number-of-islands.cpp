944f7491634a057b72e7eeefa8e81e4cc1f40074
Lakshita <98271965+lakshita157@users.noreply.github.com>
2025-06-12 16:50:06 +0530

    Time: 36 ms (19.06%), Space: 24.5 MB (17.54%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,49 +1,53 @@
 class Solution {
 public:
-     int check(int i, int j, int n, int m){
-        return i>=0  && j>=0 && i<n && j<m; 
-    } 
+    bool check(int n, int m, int i, int j){
+        return i>=0 && j>=0 && i<n && j<m;
+    }
 
-    void bfs(vector<vector<char>>& grid, int i, int j, vector<vector<int>>&vis){
+    void bfs(vector<vector<char>>& grid,vector<vector<int>>&vis, int i, int j ){ 
+        vis[i][j] =1;
+
         int n = grid.size();
         int m = grid[0].size();
-        int row[4] = {0,0,1,-1};
-        int col[4] = {1,-1,0,0};
-        vis[i][j]=1;
-        queue<pair<int,int>> q;
-        q.push({i,j});
+        queue<pair<int, int>> q;
+        q.push({i, j});
+       
+        int r[4] = {1,0,0,-1};
+        int c[4] = {0,1,-1,0};
 
         while(!q.empty()){
-            int r = q.front().first;
-            int c = q.front().second;
+            int row = q.front().first;
+            int col = q.front().second;
+
             q.pop();
-            for(int i=0; i<4; i++){
-                int newr = r+row[i];
-                int newc = c+ col[i];
-                if(check(newr, newc, n, m) && grid[newr][newc]=='1' && !vis[newr][newc]){
+
+            for(int k=0; k<4; k++){
+                int newr = row+r[k];
+                int newc = col+c[k];
+
+                if(check(n,m, newr, newc) && vis[newr][newc]==0 && grid[newr][newc] == '1'){
+                    vis[newr][newc] =1;
                     q.push({newr, newc});
-                    vis[newr][newc]=1;
                 }
             }
+
         }
 
     }
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
-        int m = grid[0].size();
+        int m= grid[0].size();
         vector<vector<int>>vis(n, vector<int>(m,0));
-        
         int cnt =0;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(grid[i][j]=='1' && vis[i][j]==0){
+                    bfs(grid, vis, i, j);
                     cnt++;
-                    bfs(grid, i, j, vis);
                 }
             }
         }
 
         return cnt;
-
     }
 };
